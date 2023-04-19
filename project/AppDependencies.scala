c00dc6cc586b1131fa44271284014d55e221986c
Peter Hazell <18005888+peter-hazell@users.noreply.github.com>
2023-04-19 16:28:39 +0100

    Update play-frontend-hmrc and use full width content for account view (#2)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.12.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "6.2.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.3.0-play-28"
   )
 
   val test: Seq[ModuleID]    = Seq(
