e1f36fb370e60d200f39b66a53d4ac25b229f02d
nemanjatomanovic <nemanja.tomanovic@gmail.com>
2023-05-12 16:57:18 +0100

    ECL-339: Added missing integration test

---
+++ it/uk/gov/hmrc/economiccrimelevyaccount/ViewYourReturnsISpec.scala
@@ -0,0 +1,29 @@
+package uk.gov.hmrc.economiccrimelevyaccount
+
+import play.api.test.FakeRequest
+import uk.gov.hmrc.economiccrimelevyaccount.base.ISpecBase
+import com.danielasfregola.randomdatagenerator.RandomDataGenerator.random
+import uk.gov.hmrc.economiccrimelevyaccount.behaviours.AuthorisedBehaviour
+import uk.gov.hmrc.economiccrimelevyaccount.generators.CachedArbitraries._
+import uk.gov.hmrc.economiccrimelevyaccount.controllers.routes
+import uk.gov.hmrc.economiccrimelevyaccount.models.ObligationData
+
+class ViewYourReturnsISpec extends ISpecBase with AuthorisedBehaviour{
+
+  s"GET ${routes.ViewYourReturnsController.onPageLoad().url}" should{
+    behave like authorisedActionRoute(routes.ViewYourReturnsController.onPageLoad())
+
+    "respond with 200 status and return correct view" in {
+      stubAuthorised()
+
+      val obligationData = random[ObligationData]
+
+      stubGetObligations(obligationData)
+
+      val result = callRoute(FakeRequest(routes.ViewYourReturnsController.onPageLoad()))
+
+      status(result) shouldBe OK
+      html(result) should include("Your Economic Crime Levy account")
+    }
+  }
+}
---
+++ it/uk/gov/hmrc/economiccrimelevyaccount/base/ObligationDataStubs.scala
@@ -0,0 +1,20 @@
+package uk.gov.hmrc.economiccrimelevyaccount.base
+
+import uk.gov.hmrc.economiccrimelevyaccount.base.WireMockHelper._
+import com.github.tomakehurst.wiremock.client.WireMock._
+import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import play.api.http.Status.OK
+import play.api.libs.json.Json
+import uk.gov.hmrc.economiccrimelevyaccount.models.ObligationData
+
+trait ObligationDataStubs { self: WireMockStubs =>
+
+  def stubGetObligations(obligationData: ObligationData): StubMapping =
+    stub(
+      get(urlEqualTo(s"/obligation-data")),
+      aResponse()
+        .withStatus(OK)
+        .withBody(Json.toJson(obligationData).toString())
+    )
+
+}
