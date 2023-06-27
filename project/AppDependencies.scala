a6844a6460e082e5a4d13fb72031d53ddba5c822
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2023-06-27 09:11:25 +0100

    ECL-453: Dependencies updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "7.15.0"
+  private val hmrcBootstrapVersion = "7.19.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % hmrcBootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.3.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.13.0-play-28"
   )
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-28"   % hmrcBootstrapVersion,
-    "org.jsoup"             % "jsoup"                    % "1.15.3",
+    "org.jsoup"             % "jsoup"                    % "1.15.4",
     "org.mockito"          %% "mockito-scala"            % "1.17.12",
     "org.scalatestplus"    %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.danielasfregola"  %% "random-data-generator"    % "2.9",
