575503cb7ccf6e5baea5b3508f20af5b2b78af10
kreddam <194923482+kreddam@users.noreply.github.com>
2025-07-15 14:41:20 +0100

    DDCYLS-7034 vulnerability fix

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "9.13.0"
+  private val hmrcBootstrapVersion = "9.16.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % hmrcBootstrapVersion,
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.6.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.7.0",
     "org.typelevel" %% "cats-core"                  % "2.13.0",
     "uk.gov.hmrc"   %% "tax-year"                   % "6.0.0"
   )
