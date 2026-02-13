c4e990189e2579f73ff9be6ead6caa1db52ae105
Athish Thayalan <120419238+AthishThayalan@users.noreply.github.com>
2026-02-13 13:11:18 +0000

    DDCYLS-8060 - Upgrade dependencies (#130)
    * Upgrade dependencies
    
    * sbt plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "9.18.0"
+  private val hmrcBootstrapVersion = "10.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % hmrcBootstrapVersion,
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.8.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.31.0",
     "org.typelevel" %% "cats-core"                  % "2.13.0",
     "uk.gov.hmrc"   %% "tax-year"                   % "6.0.0"
   )
